557276c5edad00461e0459d06c07513d2f483546
sollae-iot <84763456+sollae-iot@users.noreply.github.com>
2021-05-25 16:33:37 +0900

    Create aws-iot-configuration.js

---
+++ aws-iot-configuration.js
@@ -0,0 +1,7 @@
+var awsIotConfiguration = {
+    endpoint: 'wss://a1rwnkxviz5cvv-ats.iot.ap-southeast-1.amazonaws.com/mqtt', 
+    region: 'ap-southeast-1',
+    poolId: 'ap-southeast-1:SNT7-KD3U-JU79-T2EB',
+    host: 'a1rwnkxviz5cvv-ats.iot.ap-southeast-1.amazonaws.com'
+ };
+ module.exports = awsIotConfiguration; 
